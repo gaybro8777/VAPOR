507570f73b363c60ae92f9aa6d1fe90583f482f5
Scott Pearse <pearse@ucar.edu>
2021-05-03 13:26:37 -0600

    clang-format pre-push hook

--- apps/vaporgui/AnnotationEventRouter.cpp
+++ apps/vaporgui/AnnotationEventRouter.cpp
@@ -66,20 +66,6 @@ template<typename Out> void split(const std::string &s, char delim, Out result)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 std::vector<std::string> split(const std::string &s, char delim)
 {
     std::vector<std::string> elems;
