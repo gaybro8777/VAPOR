bea6cde4d47d0b9212d2a49a34db7aaaac08c220
Scott Pearse <pearse@ucar.edu>
2021-06-02 10:42:43 -0600

    test clang-format fix on particles branch

--- share/gitHooks/setupHooks.sh
+++ share/gitHooks/setupHooks.sh
@@ -2,12 +2,12 @@
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 HOOK_DIR=$ROOT_DIR/.git/hooks
-HOOK=$HOOK_DIR/pre-push
-ln -sf $ROOT_DIR/share/gitHooks/pre-push $HOOK
+HOOK=$HOOK_DIR/pre-commit
+ln -sf $ROOT_DIR/share/gitHooks/pre-commit $HOOK
 
 if [ -e "$HOOK" ]
 then
-    echo "pre-push hook installed in $HOOK_DIR"
+    echo "pre-commit hook installed in $HOOK_DIR"
 else
     echo "Failure: Unable to create sym-link in $HOOK_DIR"
 fi
